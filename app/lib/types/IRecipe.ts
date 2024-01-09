4cc0ae9f1e30d7dab8d84dbaef570ca345642738
Jochen Stierberger <jochen@stierberger.com>
2024-01-09 10:24:22 -0600

    Add Types

---
+++ app/lib/types/IRecipe.ts
@@ -0,0 +1,9 @@
+export default interface IRecipe {
+  created_at: string | null
+  description: string | null
+  id: string
+  name: string | null
+  updated_at: string | null
+  user_id: string | null
+
+}
